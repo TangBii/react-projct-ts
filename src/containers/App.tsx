ba20ab4d29ebd2e3eaeca246f2a5384730cbd03a
TangBii <601098499@qq.com>
2020-03-14 18:40:12 +0800

    02-引入路由

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,23 @@
 import React, {Component} from 'react'
-import {Button} from 'antd-mobile'
+import {HashRouter, Switch, Route} from 'react-router-dom'
+
+import Login from './Login'
+import Register from './Register'
+import Main from './Main'
+
+
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Button type='primary'>test</Button>
-      </div>
+      <HashRouter>
+        <Switch>
+          <Route path='/login' component={Login}/>
+          <Route path='/register' component={Register}/>
+
+          {/* 默认路由 */}
+          <Route component={Main}/>
+        </Switch>
+      </HashRouter>
     )
   }
 }
